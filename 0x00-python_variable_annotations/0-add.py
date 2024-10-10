bf7844b899503979c4ece751408a2e8af4672436
Kamogelo Jabulile Motlhale <131041491+JabbzTheCoder@users.noreply.github.com>
2024-10-10 23:43:05 +0200

    push

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
-def add(a:float,b:float ) -> float:
-    return (a + b)
+def add(a: float, b: float) -> float:
+    """
+    Adds two floating-point numbers.
+
+    Parameters:
+    a (float): The first number to be added.
+    b (float): The second number to be added.
+
+    Returns:
+    float: The sum of the two numbers.
+    
+    Example:
+    >>> add(2.5, 3.1)
+    5.6
+    """
+    return a + b
